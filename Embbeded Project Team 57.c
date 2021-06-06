e6a45e167975d9934d94b542bdb37ba67de2d0de
hussameldin171 <85431537+hussameldin171@users.noreply.github.com>
2021-06-06 15:22:19 +0200

    Update Embbeded Project Team 57.c

--- Embbeded Project Team 57.c
+++ Embbeded Project Team 57.c
@@ -5,6 +5,57 @@
 
 void SystemInit(){}
 	
+
+void UART_INT() {
+	// Initializing UART2 in PORT D 
+	//PD6 --> Rx
+	//PD7 --> Tx
+	
+	SYSCTL_RCGCUART_R |= 0x08 ;
+	SYSCTL_RCGCGPIO_R |= 0x08 ;
+	while( (SYSCTL_PRGPIO_R & 0x08) == 0 ) {}
+		
+	UART2_CTL_R  &= 0xFFFE; //Enable = 0 
+	UART2_IBRD_R = 104 ; //setting baud rate tp 9600
+	UART2_FBRD_R = 11 ;
+	UART2_LCRH_R = 0x70 ; //fifo enabled, 8 bit word length , one stop bit , no  parity check
+	UART2_CTL_R  |= 0x0201; //Enable = 1 , RXE = 1 , Receive only 
+		
+	GPIO_PORTD_AMSEL_R  &= ~0xC0 ;
+	GPIO_PORTD_DEN_R |= 0xC0 ;
+	GPIO_PORTD_AFSEL_R |= 0xC0 ;
+	GPIO_PORTD_PCTL_R = 0x11000000 ;
+}
+
+
+uint8_t ready() {
+	return ((UART2_FR_R & 0x10) == 0x10) ? 0 : 1;
+}
+
+unsigned char read() {
+	while(!ready()) {}
+	return (unsigned char)(UART2_DR_R & 0xFF) ;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 void milli_delay(int n) //software delay
 	{
     int i , j ;
